168643c3a7178120fec06ea6520bf2db89048593
Linette Jaimes <linettejaimes@gmail.com>
2020-04-24 21:49:08 -0700

    added main menu, Play button, and Quit button

---
+++ Assets/Scripts/ButtonsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*switchScenes method changes the scene to the scene corresponding to the passed integer*/
+    public void switchScenes(int x)
+    {
+        Debug.Log("You've changed scenes to: " + x);
+        SceneManager.LoadScene(x);
+    }//End of switchScenes
+
+    /*end method quits the game when called*/
+    public void end()
+    {
+        Debug.Log("You've clicked the quit button! ");
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+            Application.Quit();
+    #endif
+    }//End of End
+
+
+}//End of ButtonsController class
