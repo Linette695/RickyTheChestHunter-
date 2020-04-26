1ae268d0fc8b1d444428f2eb1683068d58fe54a2
Linette Jaimes <linettejaimes@gmail.com>
2020-04-25 19:43:05 -0700

    Player can now move left and right in first person view

--- Assets/Scripts/PlayerScripts/PlayerMove.cs
+++ Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    //Create variables for the horizontal and vertical movement input
+    [SerializeField] private string horizInputName;
+    [SerializeField] private string verInputName;
+
+    //Create reference to the character controller
+    private CharacterController charContr;
+
+    //Create variable for the character speed
+    [SerializeField] private float speed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,27 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
-}
+
+    private void Awake()
+    {
+        charContr = GetComponent<CharacterController>();    //Get the character controller directly
+    }
+
+    private void Movement()
+    {
+        //Obtain the input values for the movement in veritcal and horizontal axis 
+        float verticalInput = Input.GetAxis(verInputName) * speed;
+        float horizontalInput = Input.GetAxis(horizInputName) * speed;
+
+        Vector3 fowardMovement = transform.forward * verticalInput;
+        Vector3 rightMovement = transform.right * horizontalInput;
+
+        //Actually move the character 
+        charContr.SimpleMove(fowardMovement + rightMovement);
+    }//End of Movement method
+
+
+
+}//End of PlayerMove Class
