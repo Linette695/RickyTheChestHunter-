23d730dc994de8ef57207d57c95b54467756b2ba
Linette Jaimes <linettejaimes@gmail.com>
2020-05-01 03:44:51 -0700

    Created door animation

---
+++ Assets/Doors.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+
+    public Animator animator;
+    private  bool turqDoorOpen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        turqDoorOpen = false;
+        
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Character" ){
+            Debug.Log("Have encounter door");
+            turqDoorOpen = true;
+            doors("TurqOpen");
+
+        
+        }
+        
+    }
+
+    private void doors(string direction) {
+        animator.SetTrigger(direction);
+    }
+
+
+}
--- Assets/Scripts/PlayerScripts/PlayerItems.cs
+++ Assets/Scripts/PlayerScripts/PlayerItems.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerItems : MonoBehaviour
 {
     public Rigidbody playerRB;     //To hold the player's rigid body
+    public Text turqCount;         //The UI Text object to hold curr number of turq gems
 
     //Items to keep track of
     private int currentTurqGems = 0;
-    private
+    
 
 
 
@@ -30,7 +32,7 @@ public class PlayerItems : MonoBehaviour
         {
             currentTurqGems += 1;
             Destroy(col.gameObject);
-
+            turqCount.text = currentTurqGems.ToString();
 
         }
 
