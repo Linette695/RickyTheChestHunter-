a0354843a445777844f6cdcee1b9c25ea7091101
Linette Jaimes <linettejaimes@gmail.com>
2020-05-01 01:59:31 -0700

    Created gem collector

--- Assets/Scripts/PlayerScripts/PlayerItems.cs
+++ Assets/Scripts/PlayerScripts/PlayerItems.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerItems : MonoBehaviour
 {
+    public Rigidbody playerRB;     //To hold the player's rigid body
+
+    //Items to keep track of
+    private int currentTurqGems = 0;
+    private
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +23,24 @@ public class PlayerItems : MonoBehaviour
     {
         
     }
-}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("TurqBall"))
+        {
+            currentTurqGems += 1;
+            Destroy(col.gameObject);
+
+
+        }
+
+    }//End of OnCollisionEnter
+
+
+
+
+
+    //Getters and setters
+
+
+    }//End of PlayerItems class
