175f81cf8ae41d58eddd88267b36c72a0c0ee7f9
Linette Jaimes <linettejaimes@gmail.com>
2020-05-04 01:19:00 -0700

    Added own gem chest model to tutorial

---
+++ Assets/Scripts/GemChestScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GemChestScript : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject squareTrigger;
+
+    public Text numKeys;    //Displayed integer on canvas (the count the player can see)
+    public int keysNeeded;  //To hold the number of keys needed for this level's chest
+    public int nextScene;   //To indicate the next scene loaded after the chest is opened
+
+    private int numOfKeys;  //Will hold the number of keys player has collected in int form
+    private bool chestOpen; //To keep track if chest has been opened
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chestOpen = false;
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        numOfKeys = int.Parse(numKeys.text);    //Convert to integer to get number of keys collected
+        if (col.gameObject.tag == "Character" &&  numOfKeys == keysNeeded)
+        {
+            chestOpen = true;
+            openChest("ChestOpen");
+           
+
+
+        }
+
+    }
+
+    private void openChest(string direction)
+    {
+        
+        animator.SetTrigger(direction);
+        StartCoroutine(Helper());
+    }
+
+   IEnumerator Helper() {
+        yield return new WaitForSeconds(0.30f);
+        switchScenes(nextScene);
+
+
+    }
+
+
+    public void switchScenes(int x)
+    {
+        Debug.Log("You've changed scenes to: " + x);
+        SceneManager.LoadScene(x);
+    }//End of switchScenes
+
+}//End of GemChestScript class
