01f4bff64e75cf09d3ade25b313999c763d30063
Linette Jaimes <linettejaimes@gmail.com>
2020-05-09 19:59:04 -0700

    Added new chest animation

--- Assets/Scripts/GemChestScript.cs
+++ Assets/Scripts/GemChestScript.cs
@@ -29,7 +29,7 @@ public class GemChestScript : MonoBehaviour
         if (col.gameObject.tag == "Character" &&  numOfKeys == keysNeeded)
         {
             chestOpen = true;
-            openChest("ChestOpen");
+            openChest("chestOpen");
            
 
 
@@ -39,14 +39,14 @@ public class GemChestScript : MonoBehaviour
 
     private void openChest(string trig)
     {
-        impAnimation.Play("ChestOpen");
+        impAnimation.Play(trig);
        
 
         StartCoroutine(Helper());
     }
 
    IEnumerator Helper() {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
         switchScenes(nextScene);
 
 
