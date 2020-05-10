f09a4ad1093de4c7a2c85f411fb81f3b6f0f8bd7
Linette Jaimes <linettejaimes@gmail.com>
2020-05-09 20:46:38 -0700

    Added door + animation on level2

---
+++ Assets/Animation/L2FrontDoors.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2FrontDoors : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject squareTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Character")
+        {
+            
+            doors("openFrontDoor");
+
+
+        }
+
+    }
+
+    private void doors(string direction)
+    {
+        animator.SetTrigger(direction);
+        
+    }
+
+
+}//End of L2FrontDoors class
+
